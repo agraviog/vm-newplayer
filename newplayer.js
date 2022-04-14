c1c6127ec54e54de99ea80a93c55314ea4d2b6ae
Genesis Agravio <agraviog@gmail.com>
2022-04-14 13:26:48 +0800

    dropbox link

--- newplayer.js
+++ newplayer.js
@@ -87,7 +87,9 @@ Webflow.push(function () {
   });
 
   plyApple.onclick = function () {
-    waveForm.load("audio/an_apple_a_day_original.mp3");
+    waveForm.load(
+      "https://uc17744491a50b9809900979ba18.dl.dropboxusercontent.com/cd/0/inline/BjVjpR2cY1pFoaNP1A4zpHadC-c1HKvRRn9COrw8USXM-JIk45DZLZN9nCbwsXvYow-K5afdh4nOACQYE-zHXzaUENulbRh3_yroFC14ryrvu4DMhlvENouourveyyId01BkncaN0h7uUoVZ5XKbRPbkvRcSv4pPQvJdMwdUDy4N9g/file#"
+    );
     $(".control_play--nplayer").removeClass("control_disable");
     $(".checkbox--nplayer").removeClass("w--redirected-checked");
     waveForm.on("ready", function () {
@@ -101,7 +103,9 @@ Webflow.push(function () {
   };
 
   plyHarmony.onclick = function () {
-    waveForm.load("audio/harmony_original.mp3");
+    waveForm.load(
+      "https://uc91552616a4413b58faff2dbf12.dl.dropboxusercontent.com/cd/0/inline/BjWdakk45icro_Amnuo4ghnQP_rQIJ6imnST0NJq_a_UwAhLMqjS06PB7QE6UkPS6j8oRrwZdrWsgSsQ-LKUEpMs4LFEvoFsrHLluIB04rs9gkHc46BMl3Ki-_wAbsFKXpJ1w_2omJHvkhvV8U8Xh9r2RozsuF7SElMrgDAxm7kWLg/file#"
+    );
     $(".control_play--nplayer").removeClass("control_disable");
     $(".checkbox--nplayer").removeClass("w--redirected-checked");
     waveForm.on("ready", function () {
@@ -115,7 +119,9 @@ Webflow.push(function () {
   };
 
   plyPhone.onclick = function () {
-    waveForm.load("audio/phone_effect_original.mp3");
+    waveForm.load(
+      "https://uc7827a9b3e6aa599093626e4d39.dl.dropboxusercontent.com/cd/0/inline/BjXlZeeATDc0b6-uzDyTqlnZrOwAAwNpq6O6TJka6RIlOHtBdUOmmnjk5teK4B69ZUHNxp1Tvu6rUrWRZ4aupTgcdop-trEoxWQDib4DUaWrSezO0nbPdl9gKicTT4qjQbAEZFqalnPVyppZI1SsMXxDj7WpOywSavAcD0lUg4uVAQ/file#"
+    );
     $(".control_play--nplayer").removeClass("control_disable");
     $(".checkbox--nplayer").removeClass("w--redirected-checked");
     waveForm.on("ready", function () {
@@ -129,7 +135,9 @@ Webflow.push(function () {
   };
   plyApple.click();
 
-  var audioOne = new Audio("audio/an_apple_a_day_vm.mp3");
+  var audioOne = new Audio(
+    "https://uc29af9bb29daf04078cd93ec6e9.dl.dropboxusercontent.com/cd/0/inline/BjXnx8kgsAUgJB3zOjcavMY1pksEAvuH-Zno2kWIbD48cF1O1weVPBnq5G78_z2j1Lw5IEmS5CyffiVS-IjIvIs0rmdV9J545jLj7g8DPxVIl45dqQXykRFangca59stgxyW9nGAWqa-eIIsqB9341Aur0MeynLnYQh2tzEkVeITag/file#"
+  );
   audioOne.muted = true;
 
   $(".control_play--nplayer").on("click", function () {
@@ -155,17 +163,23 @@ Webflow.push(function () {
       var time = waveForm.getCurrentTime();
       if (time !== 0) {
         if (playing === 1) {
-          waveForm.load("audio/an_apple_a_day_original.mp3");
+          waveForm.load(
+            "https://uc17744491a50b9809900979ba18.dl.dropboxusercontent.com/cd/0/inline/BjVjpR2cY1pFoaNP1A4zpHadC-c1HKvRRn9COrw8USXM-JIk45DZLZN9nCbwsXvYow-K5afdh4nOACQYE-zHXzaUENulbRh3_yroFC14ryrvu4DMhlvENouourveyyId01BkncaN0h7uUoVZ5XKbRPbkvRcSv4pPQvJdMwdUDy4N9g/file#"
+          );
           waveForm.on("ready", function () {
             waveForm.play(time);
           });
         } else if (playing === 2) {
-          waveForm.load("audio/harmony_original.mp3");
+          waveForm.load(
+            "https://uc7827a9b3e6aa599093626e4d39.dl.dropboxusercontent.com/cd/0/inline/BjXlZeeATDc0b6-uzDyTqlnZrOwAAwNpq6O6TJka6RIlOHtBdUOmmnjk5teK4B69ZUHNxp1Tvu6rUrWRZ4aupTgcdop-trEoxWQDib4DUaWrSezO0nbPdl9gKicTT4qjQbAEZFqalnPVyppZI1SsMXxDj7WpOywSavAcD0lUg4uVAQ/file#"
+          );
           waveForm.on("ready", function () {
             waveForm.play(time);
           });
         } else if (playing === 3) {
-          waveForm.load("audio/phone_effect_original.mp3");
+          waveForm.load(
+            "https://uc7827a9b3e6aa599093626e4d39.dl.dropboxusercontent.com/cd/0/inline/BjXlZeeATDc0b6-uzDyTqlnZrOwAAwNpq6O6TJka6RIlOHtBdUOmmnjk5teK4B69ZUHNxp1Tvu6rUrWRZ4aupTgcdop-trEoxWQDib4DUaWrSezO0nbPdl9gKicTT4qjQbAEZFqalnPVyppZI1SsMXxDj7WpOywSavAcD0lUg4uVAQ/file#"
+          );
           waveForm.on("ready", function () {
             waveForm.play(time);
           });
